756e5b508dd8739c66acf199fae32b73ee9619fa
המחשב של הילה <hilazoha@gmail.com>
2026-03-29 19:24:53 +0300

    commit 2

---
+++ app/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.ex13;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author Hila
+ * @version 1.0
+ * @since 2026-03-29
+ * * The MainActivity class manages the background color of the layout based on 
+ * the selected RadioButton and the state of the Switch.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * The main layout of the activity whose background color changes.
+     */
+    private LinearLayout m;
+
+    /**
+     * The radio buttons for color selection.
+     */
+    private RadioButton rB1, rB2, rB3, rB4;
+
+    /**
+     * The switch that toggles between immediate and button-click changes.
+     */
+    private Switch sW;
+
+    /**
+     * The group containing all the radio buttons.
+     */
+    private RadioGroup colorGroup;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        m = findViewById(R.id.main);
+        rB1 = findViewById(R.id.rB1);
+        rB2 = findViewById(R.id.rB2);
+        rB3 = findViewById(R.id.rB3);
+        rB4 = findViewById(R.id.rB4);
+        sW = findViewById(R.id.sW);
+        colorGroup = findViewById(R.id.colorGroup);
+
+        m.setBackgroundColor(Color.GRAY);
+    }
+
+    /**
+     * Handles the click event on any RadioButton.
+     * Changes the background color immediately if the Switch is checked.
+     *
+     * @param view The RadioButton that was clicked.
+     */
+    public void onRadioButtonClicked(View view) {
+        if (sW.isChecked() == true) {
+
+            if (view.getId() == R.id.rB1) {
+                m.setBackgroundColor(Color.RED);
+            } else if (view.getId() == R.id.rB2) {
+                m.setBackgroundColor(Color.BLUE);
+            } else if (view.getId() == R.id.rB3) {
+                m.setBackgroundColor(Color.GREEN);
+            } else if (view.getId() == R.id.rB4) {
+                m.setBackgroundColor(Color.YELLOW);
+            }
+
+        }
+    }
+
+}
+
